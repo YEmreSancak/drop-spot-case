6339a9116d5b37bf50f92dc61f774590613297aa
YEmreSancak <yunus.emre.sancak99@gmail.com>
2025-11-09 02:41:33 +0300

    test(ui): add initial home page render test

---
+++ frontend/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
+};
